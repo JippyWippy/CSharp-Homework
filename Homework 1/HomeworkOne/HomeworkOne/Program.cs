a5a1944ff27703a8b29df1269aa549e10fc8315e
JippyWippy <ilyaseelev76@gmail.com>
2023-03-20 00:59:45 +0700

    Домашнее задание №1

---
+++ Homework 1/HomeworkOne/HomeworkOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Доброго дня! Введите своё имя: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Привет, { name}!");
+        }
+    }
+}
